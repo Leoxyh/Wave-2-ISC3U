126546cef6cb752a5c156ea4c91419c1fa5d85c2
Leoxyh <58529404+Leoxyh@users.noreply.github.com>
2020-04-18 17:07:54 -0400

    Add files via upload

---
+++ Wave 2-Even or Odd.py
@@ -0,0 +1,12 @@
+import math
+
+# input
+
+number = input("Enter your number: ")
+number = int(number)
+
+# calculate whether the number is even or odd
+if number % 2 == 0:
+    print("This number is even")
+else:
+    print("This number is odd")
