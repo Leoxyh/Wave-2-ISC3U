be461be421014e7bb2210432f1fd921575e0c270
Leoxyh <58529404+Leoxyh@users.noreply.github.com>
2020-04-23 20:09:43 -0400

    Add files via upload

---
+++ Wave 2-Month to Days.py
@@ -0,0 +1,20 @@
+import math
+import calendar
+
+# Inputs
+
+print("List for months: January, February, March, April, May, June, July, August, September, October, November, December")
+print("Please capitalize your first letter")
+month = input("Enter your month: ")
+month = str(month)
+
+# Check the mouth for days
+
+if month in ("January", "March", "May", "July", "August", "October", "December"):
+    print(str(month), "have 31 days")
+elif month in ("April", "June", "September", "November"):
+    print(str(month), "have 30 days")
+elif month == "February":
+    print(str(month), "have 28 or 29 days")
+else:
+    print("Undefined month name")
